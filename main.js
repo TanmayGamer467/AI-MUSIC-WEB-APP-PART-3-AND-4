a177f131697b19d0ad408a3a8754d4defb6e9606
TanmayGamer467 <109866407+TanmayGamer467@users.noreply.github.com>
2023-01-04 11:40:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+song1="";
+song2="";
+song1_status="";
+song2_status="";
+scoreRightWrist = 0;
+ scoreLeftWrist = 0;
+  rightWristX = 0;
+   rightWristY = 0; 
+   leftWristX = 0;
+    leftWristY = 0;
+
+    function preload(){
+        song1=loadSound("song1.mp3");
+        song2=loadSound("song2.mp3");
+    }
+
+    function setup() { canvas = createCanvas(600, 500);
+         canvas.center();
+          video = createCapture(VIDEO);
+           video.hide();
+            poseNet = ml5.poseNet(video, modelLoaded);
+             poseNet.on('pose', gotPoses); }
+     function modelLoaded() {
+         console.log('PoseNet Is Initialized'); }
+
+         function draw(){
+            image(video,0,0,600,500);
+            song1_status=song1.isPlaying();
+            console.log(song1);
+            if(scoreLeftWrist>0.2){
+              circle(leftWristX,leftWristY,20);
+              song2.stop();
+              if(song1==false){
+                song1.play();
+              }
+              else{
+                document.getElementById("song").innerHTML="Song Name:Maan Meri Jaan ";
+              }
+            }
+            song2_status=song2.isPlaying();
+            fill('red');
+            stroke('red');
+         }
+
+  function gotPoses(results){
+if(results.length>0){
+  scoreLeftWrist=results[0].pose.keypoints[9].score;
+  console.log(scoreLeftWrist);
+  scoreRightWrist=results[0].pose.keypoints[10].score;
+     
+     leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX=" + leftWristX + "leftWristY=" + leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX=" + rightWristX + "rightWristY=" + rightWristY);
+}
+  }       
